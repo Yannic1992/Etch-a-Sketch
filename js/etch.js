4fcecfd4ad6e9ac6d95d103fc306cbd9608ea696
Yannic1992 <yannic1992@gmx.net>
2023-01-11 14:46:57 +0100

    Add a basic grid pattern using CSS Grid

--- js/etch.js
+++ js/etch.js
@@ -4,9 +4,8 @@ for(let i = 0; i < 256; i++){
     const grid = document.createElement('div');
     grid.classList.add('grid');
     container.appendChild(grid);
-    
+    /*Remove comment here and at float: left; in css file for using float/clear
     if(i>0 && i%16==0){
-        /*Remove comment from float: left; in css file
-        grid.style.clear = 'left'; */
-    }
+        grid.style.clear = 'left';
+    }*/
 }
