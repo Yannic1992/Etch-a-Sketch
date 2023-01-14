3d048e7e6645d800716f2caf0e0f121714da012b
Yannic1992 <yannic1992@gmx.net>
2023-01-14 15:24:34 +0100

    Add a button that changes grid resolution

--- js/etch.js
+++ js/etch.js
@@ -1,21 +1,47 @@
 const container = document.querySelector(".container");
+let girdNumber = 16;
 
-for(let i = 0; i < 256; i++){
-    const grid = document.createElement('div');
-    grid.classList.add("grid");
-    grid.setAttribute("id", "grid"+i);
-    container.appendChild(grid);
-    /*Remove comment here and at float: left; in css file for using float/clear
-    if(i>0 && i%16==0){
-        grid.style.clear = "left";
-    }*/
+function createGrid(){
+    for(let i = 0; i < girdNumber*girdNumber; i++){
+        const grid = document.createElement('div');
+        grid.style.width = 480/girdNumber + "px";
+        grid.style.height = 480/girdNumber + "px";
+        grid.classList.add("grid");
+        grid.setAttribute("id", "grid"+i);
+        container.appendChild(grid);
+        /*Remove comment here and at float: left; in css file for using float/clear
+        if(i>0 && i%16==0){
+            grid.style.clear = "left";
+        }*/
+    }
 }
 
+function deleteGrid(){
+    const grid = Array.from(document.querySelectorAll(".grid"));
+    console.log(grid);
+    grid.forEach(gridBox => {
+        console.log(gridBox.id);
+        document.getElementById(gridBox.id).remove();
+        //document.getElementById(gridBox.id).style.backgroundColor = "red";
+    });
+}
 
-const gridboxes = Array.from(document.querySelectorAll(".grid"));
-gridboxes.forEach(box => {
-    //console.log("ID: " + box.id);
-    box.addEventListener("mouseover", function() {
-        //console.log("ID Neu: " + box.id);
-        document.getElementById(box.id).style.backgroundColor = "black";
-})});
+function addEffect() {
+    const gridboxes = Array.from(document.querySelectorAll(".grid"));
+    gridboxes.forEach(box => {
+        //console.log("ID: " + box.id);
+        box.addEventListener("mouseover", function() {
+            //console.log("ID Neu: " + box.id);
+            document.getElementById(box.id).style.backgroundColor = "black";
+    })});
+}
+
+function onClick(){
+    girdNumber = prompt("Please enter number of squares per side", "16");
+    deleteGrid();
+    createGrid();
+    addEffect();
+}
+
+createGrid();
+addEffect();
