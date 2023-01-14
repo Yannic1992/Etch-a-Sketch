4d355f3657d15c5970236474913054a601af98d0
Yannic1992 <yannic1992@gmx.net>
2023-01-14 15:49:11 +0100

    Remove unused code and comments

--- js/etch.js
+++ js/etch.js
@@ -9,29 +9,20 @@ function createGrid(){
         grid.classList.add("grid");
         grid.setAttribute("id", "grid"+i);
         container.appendChild(grid);
-        /*Remove comment here and at float: left; in css file for using float/clear
-        if(i>0 && i%16==0){
-            grid.style.clear = "left";
-        }*/
     }
 }
 
 function deleteGrid(){
     const grid = Array.from(document.querySelectorAll(".grid"));
-    console.log(grid);
     grid.forEach(gridBox => {
-        console.log(gridBox.id);
         document.getElementById(gridBox.id).remove();
-        //document.getElementById(gridBox.id).style.backgroundColor = "red";
     });
 }
 
 function addEffect() {
     const gridboxes = Array.from(document.querySelectorAll(".grid"));
     gridboxes.forEach(box => {
-        //console.log("ID: " + box.id);
         box.addEventListener("mouseover", function() {
-            //console.log("ID Neu: " + box.id);
             document.getElementById(box.id).style.backgroundColor = "black";
     })});
 }
